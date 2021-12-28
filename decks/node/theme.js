4b79a7f30ab2fff201b28f2b91a9ff00ca33313d
toyobayashi <lifenglin314@outlook.com>
2021-12-28 18:55:25 +0800

    node

---
+++ decks/node/theme.js
@@ -0,0 +1,5 @@
+import { vsDark as theme } from '@code-surfer/themes'
+
+theme.styles.CodeSurfer.code.fontFamily = '"Cascadia Code", Consolas, monaco, "Courier New", monospace'
+
+export default theme
