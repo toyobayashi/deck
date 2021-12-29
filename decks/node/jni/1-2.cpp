4abb2b33a3cd910c9780a689399a27cba7d2b9b0
toyobayashi <lifenglin314@outlook.com>
2021-12-29 11:16:31 +0800

    node

--- decks/node/jni/1-2.cpp
+++ decks/node/jni/1-2.cpp
@@ -1,10 +1,10 @@
 extern "C" JNIEXPORT jint JNICALL
 Java_package_NodeJs_initialize(JNIEnv *env, jclass clazz) {
   start_redirecting_stdout_stderr();
-  return NodeInstance::Initialize();
+  return NodeJs::Initialize();
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_package_NodeJs_shutdown(JNIEnv *env, jclass clazz) {
-  NodeInstance::Shutdown();
+  NodeJs::Shutdown();
 }
