84491740d2cb48cdc8035dfdaf2acd81b91de179
toyobayashi <lifenglin314@outlook.com>
2022-02-17 18:05:48 +0800

    vue3change

---
+++ decks/vue3change/theme.js
@@ -0,0 +1,8 @@
+import { vsDark as theme } from '@code-surfer/themes'
+
+theme.styles.CodeSurfer.code.fontFamily = '"Cascadia Code", Consolas, monaco, "Courier New", monospace'
+// theme.styles.CodeSurfer.subtitle.width = '25%'
+// theme.styles.CodeSurfer.subtitle.left = '-0.8em'
+// theme.styles.CodeSurfer.subtitle.fontSize = '0.8em'
+
+export default theme
