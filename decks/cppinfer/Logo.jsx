402d009c28ef20f281ba3c064ef9ced0c694c636
toyobayashi <lifenglin314@outlook.com>
2022-04-06 16:32:05 +0800

    cppinfer

---
+++ decks/cppinfer/Logo.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+import cpp from './assets/cpp.svg'
+
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '60%'
+}
+
+export default function () {
+  return (
+    <div style={style}>
+      <img src={cpp} alt='webpack' width={300} />
+    </div>
+  )
+}
